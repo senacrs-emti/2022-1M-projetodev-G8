d87a63f1c9216f463e07f7fd942fd6aff27fe2ea
GiovanaRossi <giovanarossi.gi@gmail.com>
2022-11-10 09:26:32 -0300

    Atualização 3

--- História 2/inicio2.php
+++ História 2/inicio2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="história2.css">
+    <link rel="stylesheet" href="historia2.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>História 2</title>
 </head>
@@ -20,7 +20,7 @@
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, excepturi eum sunt, nam molestiae perferendis quibusdam dolorem sint labore dignissimos pariatur error veritatis facere sequi inventore soluta? Dolores, perferendis sed.
     </p>
     <a href="#" class="btn2 btn-lg">OK</a>
-  </div>
+  </div><b></b>
 </div>
 </body>
 </html>
